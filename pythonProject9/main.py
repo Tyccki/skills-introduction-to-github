65a32490f97f95940cf5b0b6aff0f7a1d125c99e
Tyccki <mansqqqww@gmail.com>
2025-01-19 21:42:50 +0500

    Add files via upload

---
+++ pythonProject9/main.py
@@ -0,0 +1,37 @@
+#for i in range(1, 8, 2):
+ #   print(i)
+
+#count = 0
+#word = "Hello World!"
+#for i in word:
+  #  if i == "l":
+   #    count += 1
+
+
+#print("Count:",count)
+
+#isHasCar = True
+ #while isHasCar:
+#    print(isHasCar)
+   # if input('enterdate: ') == "stop":
+    #    isHasCar = False
+      #  print ('base was stopped')
+
+
+#for i in range(1, 11):
+    #if i  == 5:
+     #   break
+
+
+  #  if i % 2 ==0:
+#     continue
+#      print(i)
+
+#found = None
+#for i in "Hello!":
+#    if i == "l":
+ #       found = True
+ #       break
+#else:
+#    found = False
+#print(found)
